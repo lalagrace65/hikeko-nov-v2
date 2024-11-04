867a3ca0a975bb09aa88e8991dd328599425bcfb
lalagrace65 <lalagrace65@gmail.com>
2024-11-04 22:08:03 +0800

    edit the cors

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 app.use(cors({
     credentials: true,
-    origin: ['https://hikeko-nov-v2-client.onrender.com/'],
+    origin: ['https://hikeko-nov-v2-client.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
