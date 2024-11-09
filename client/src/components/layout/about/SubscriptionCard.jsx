acc4362bf09f9e08d62180ac5b9c881944e2bc37
A1lsn <allisonperez055@gmail.com>
2024-11-10 06:59:09 +0800

    pricing card (SubscritionCard.jsx) added on aboutPage.jsx

---
+++ client/src/components/layout/about/SubscriptionCard.jsx
@@ -0,0 +1,154 @@
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+  } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
+   
+function CheckIcon() {
+return (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    className="h-3 w-3"
+    >
+    <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+    />
+    </svg>
+);
+}
+
+export function PricingCard() {
+const navigate = useNavigate();
+
+const handleCardButton = () => {
+    navigate('/travelAgencySignUp'); 
+    };
+return (
+    <div className="flex justify-center items-center mt-20 gap-10">
+    {/*basic plan subscription*/}
+        <Card variant="gradient" className="w-full max-w-[20rem] p-8 bg-cardPrice shadow-xl hover:scale-[1.02] ">
+            <CardHeader
+                floated={false}
+                shadow={false}
+                color="transparent"
+                className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center text-primary"
+                >
+                <Typography variant="small" className="font-normal uppercas">BASIC PLAN</Typography>
+                <Typography
+                    variant="h1"
+                    className="mt-6 flex justify-center gap-1 text-7xl font-normal"
+                >
+                    <span className="mt-2 text-4xl">₱</span>0{" "}
+                    <span className="self-end text-4xl"></span>
+                </Typography>
+            </CardHeader>
+            <CardBody className="p-0">
+                <ul className="flex flex-col gap-4">
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">1 Admin Account</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">3 Staff Accounts</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">Up to 6 Events per month</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">3 months free plan</Typography>
+                    </li>
+                </ul>
+            </CardBody>
+            <CardFooter className="mt-12 p-0">
+                <Button
+                    size="lg"
+                    className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100 bg-primary "
+                    ripple={false}
+                    fullWidth={true}
+                    onClick={handleCardButton}
+                >
+                    Get Started
+                </Button>
+            </CardFooter>
+        </Card>
+        {/*Premium plan subscription*/}
+        <Card variant="gradient" className="w-full max-w-[20rem] p-8 bg-cardPrice shadow-xl hover:scale-[1.02] ">
+            <CardHeader
+                floated={false}
+                shadow={false}
+                color="transparent"
+                className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center text-primary"
+                >
+                <Typography variant="small" className="font-normal uppercas">PREMIUM PLAN</Typography>
+                <Typography
+                    variant="h1"
+                    className="mt-6 flex justify-center gap-1 text-7xl font-normal"
+                >
+                    <span className="mt-2 text-4xl">₱</span>120{" "}
+                    <span className="self-end text-sm">/6 months</span>
+                </Typography>
+            </CardHeader>
+            <CardBody className="p-0">
+                <ul className="flex flex-col gap-4">
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">1 Admin Account</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">5 Staff Accounts</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20  bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">Up to 10 Events per month</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                    <span className="rounded-full border border-white/20 bg-primary p-1 text-white">
+                        <CheckIcon />
+                    </span>
+                    <Typography className="font-normal">2 Ads per month</Typography>
+                    </li>
+                </ul>
+            </CardBody>
+            <CardFooter className="mt-12 p-0">
+                <Button
+                    size="lg"
+                    className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100 bg-primary "
+                    ripple={false}
+                    fullWidth={true}
+                    onClick={handleCardButton}
+                >
+                    Get Started
+                </Button>
+            </CardFooter>
+        </Card>
+    </div>
+ );
+}
--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,8 +1,18 @@
+import { PricingCard } from "@/components/layout/about/SubscriptionCard";
+import { useEffect } from "react";
+
 export default function AboutPage() {
+    useEffect(() => {
+        // Scroll to top on component mount
+        window.scrollTo(0, 0);
+      }, []);
     return (
         <div>
             <div className="flex justify-center items-center bg-about p-20">
                 <h5 className="text-white font-bold text-5xl">DISCOVER THE WONDERS OF NATURE WITH HIKEKO</h5>
+            </div>
+            <div className="">
+                <PricingCard />
             </div>
         </div>
     )
