e276cd1239433fe0c266ed86ee30875a5cd74cd3
lalagrace65 <lalagrace65@gmail.com>
2024-11-26 00:48:04 +0800

    trust proxy

--- api/index.js
+++ api/index.js
@@ -59,40 +59,15 @@ app.use(
         crossOriginEmbedderPolicy: true,
     })
 );
+app.set('trust proxy', 1);
 
-// Set middleware of CORS 
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://hikeko-nov-v2-client.onrender.com"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
-  
-    next();
-  });
 
-// Set the 'Access-Control-Allow-Origin' header explicitly
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://hikeko-nov-v2-client.onrender.com, http://localhost:5174');
-    next();
-});
-
+// CORS configuration
 app.use(cors({
     credentials: true,
     origin: ['https://hikeko-nov-v2-client.onrender.com', 'http://localhost:5174'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers']
 }));
 
 // Connect to MongoDB
