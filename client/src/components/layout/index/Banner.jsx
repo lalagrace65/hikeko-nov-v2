3ea96a07195fe263e5dc5459c40ca1c372855117
A1lsn <allisonperez055@gmail.com>
2024-11-29 10:46:09 +0800

    fix minor UI issue in banner

--- client/src/components/layout/index/Banner.jsx
+++ client/src/components/layout/index/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
                 className="absolute inset-0 w-full h-full object-cover object-bottom"
                 style={{ objectPosition: "50% 60%" }}
             />
-            <div class="absolute inset-0 bg-black/50 opacity-20"></div>
+            <div className="absolute inset-0 bg-black/50 opacity-30"></div>
             <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-2 sm:px-4 lg:px-6">
                 <Typography className="text-2xl sm:text-4xl lg:text-6xl font-bold text-center">
                     Discover your next hiking adventure
