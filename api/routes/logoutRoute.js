024e6efa8fd89c29a77277bb96e7864b75d929ce
A1lsn <allisonperez055@gmail.com>
2024-11-30 01:29:39 +0800

    logout now removes user token

--- api/routes/logoutRoute.js
+++ api/routes/logoutRoute.js
@@ -4,14 +4,7 @@ const router = express.Router();
 
 // Logout route
 router.post('/logout', (req, res) => {
-    res.cookie('token', '', {
-        httpOnly: true,         // Make sure the cookie can't be accessed from JavaScript
-        secure: process.env.NODE_ENV === 'production', // Ensure it's only sent over HTTPS in production
-        sameSite: 'None',       // Allow cross-origin requests (if needed)
-        expires: new Date(0),  // Set expiration date to the past to remove it
-    });
-
-    return res.json({ message: 'Logged out successfully' });
+    res.cookie('token', '', { expires: new Date(0) }).json({ message: 'Logged out' });
 });
 
 module.exports = router;
