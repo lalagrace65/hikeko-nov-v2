e821364a009e17484aadbd8e3a9fc50b329a82f3
A1lsn <allisonperez055@gmail.com>
2024-11-29 10:50:57 +0800

    added console log in googleApi. constGooglePlace is undefined

--- client/src/Shared/GlobalApi.jsx
+++ client/src/Shared/GlobalApi.jsx
@@ -2,6 +2,7 @@ import { baseUrl } from '@/Url';
 import axios from 'axios';
 
 const getGooglePlace = (category, radius, lat, lng) => {
+    console.log("Category:", category, "Radius:", radius, "Lat:", lat, "Lng:", lng);
     return axios.get(`${baseUrl}/api/google-place?` + 
         'category=' + category + 
         '&radius=' + radius + 
