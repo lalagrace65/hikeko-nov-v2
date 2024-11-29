54c2b538b1e50c32ff28075cd557f253f7c95671
A1lsn <allisonperez055@gmail.com>
2024-11-30 01:42:01 +0800

    explore dropdown closes onclick to tabs

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -216,8 +216,12 @@ export default function Header() {
                             </Link>
                             {exploreDropdownOpen && (
                                 <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg py-2 border">
-                                    <Link to="/trails" className="block px-4 py-2 text-gray-700 hover:bg-gray-200">Tour & Packages</Link>
-                                    <Link to="/forum" className="block px-4 py-2 text-gray-700 hover:bg-gray-200">Community</Link>
+                                    <Link to="/trails" 
+                                        onClick={() => setExploreDropdownOpen(false)}
+                                        className="block px-4 py-2 text-gray-700 hover:bg-gray-200">Tour & Packages</Link>
+                                    <Link to="/forum" 
+                                        onClick={() => setExploreDropdownOpen(false)}
+                                        className="block px-4 py-2 text-gray-700 hover:bg-gray-200">Community</Link>
                                 </div>
                             )}
                         </div>
