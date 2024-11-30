5adc8c61ac44d4456e62ed5d8e34c4da972561cd
A1lsn <allisonperez055@gmail.com>
2024-11-30 18:49:54 +0800

    logout test

--- api/routes/logoutRoute.js
+++ api/routes/logoutRoute.js
@@ -5,15 +5,14 @@ const router = express.Router();
 
 // Logout route
 router.post('/logout', (req, res) => {
-  // Clear the cookie storing the JWT token
-  res.clearCookie('user_token', {
-    httpOnly: true, // Ensures the cookie is accessible only by the server
-    secure: true,   // Ensures the cookie is sent only over HTTPS
-    sameSite: 'None', // Necessary for cross-origin requests
-  });
-
-  // Respond with a success message
-  res.status(200).json({ message: 'Logged out successfully' });
+    const token = jwt.sign({_id: req.user._id}, process.env.JWT_SECRET, {expiresIn: '0ms'});
+    // res.cookie('user_token', 'null', {maxAge: -1, httpOnly: true, sameSite : "None", secure: true});
+    // res.cookie('user_tokens', 'null', {maxAge: -1, httpOnly: true, sameSite : "None", secure: true});
+    res.clearCookie('user_token', {
+      httpOnly: true,
+      secure: true, 
+      sameSite: 'None', 
+    });
 });
 
 module.exports = router;
