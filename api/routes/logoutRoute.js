8988afae7ed35ac0dfea8c417fa255b6c1598acf
A1lsn <allisonperez055@gmail.com>
2024-11-30 18:40:52 +0800

    logout modified

--- api/routes/logoutRoute.js
+++ api/routes/logoutRoute.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 // Logout route
 router.post('/logout', (req, res) => {
-    res.cookie('token', '', { expires: new Date(0) }).json({ message: 'Logged out' });
+  // Clear the cookie storing the JWT token
+  res.clearCookie('user_token', {
+    httpOnly: true, // Ensures the cookie is accessible only by the server
+    secure: true,   // Ensures the cookie is sent only over HTTPS
+    sameSite: 'None', // Necessary for cross-origin requests
+  });
+
+  // Respond with a success message
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 module.exports = router;
