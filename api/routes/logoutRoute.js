d6e6a4c628a2e2855e5c93e0c9917efc06cfb1fa
A1lsn <allisonperez055@gmail.com>
2024-11-30 10:22:10 +0800

    logout test2

--- api/routes/logoutRoute.js
+++ api/routes/logoutRoute.js
@@ -4,14 +4,7 @@ const router = express.Router();
 
 // Logout route
 router.post('/logout', (req, res) => {
-    res.cookie('token', '', { 
-        expires: new Date(0), 
-        httpOnly: true, 
-        secure: process.env.NODE_ENV === 'production',  // Set secure cookie only for production
-        domain: 'https://hikeko-client.vercel.app/',  // Adjust as necessary
-        path: '/'
-    }).json({ message: 'Logged out' });
-    
+    res.cookie('token', '', { expires: new Date(0) }).json({ message: 'Logged out' });
 });
 
 module.exports = router;
